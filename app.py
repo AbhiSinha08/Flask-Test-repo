c93e34a6dd76aea27aed7c52c4179dd12a9357e5
Abhinav Sinha <64038655+AbhiSinha08@users.noreply.github.com>
2021-09-11 00:12:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import os
+os.chdir(__file__.replace(os.path.basename(__file__), ''))
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.getcwd()
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    var_name = request.form['name']
+    var_age = request.form['age']
+    image = request.files['image']
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], 'static/image1.jpg'))    
+    return render_template("hello.html", na=var_name, ag=var_age)
+
+@app.route('/search')
+def search():
+    imagePaths = ["../static/image1.jpg"]
+    return render_template("search.html", imagepaths=imagePaths)
+
+if __name__ == '__main__':
+    app.run()

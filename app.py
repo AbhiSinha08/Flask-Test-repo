7879eca5f64d983b2bb5596c740bec3f500a11af
Abhinav Sinha <64038655+AbhiSinha08@users.noreply.github.com>
2021-09-11 12:31:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from db import *
 import os
 os.chdir(__file__.replace(os.path.basename(__file__), ''))
 app = Flask(__name__)
@@ -13,13 +14,14 @@ def hello():
     var_name = request.form['name']
     var_age = request.form['age']
     image = request.files['image']
-    image.save(os.path.join(app.config['UPLOAD_FOLDER'], 'static/image1.jpg'))    
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], 'static/image1.jpg'))
+    addEntry(var_name, var_age)  
     return render_template("hello.html", na=var_name, ag=var_age)
 
 @app.route('/search')
 def search():
     imagePaths = ["../static/image1.jpg"]
-    return render_template("search.html", imagepaths=imagePaths)
+    return render_template("search.html", result=show(), imagepaths=imagePaths)
 
 if __name__ == '__main__':
     app.run()
---
+++ db.py
@@ -0,0 +1,27 @@
+import os
+import psycopg2
+os.chdir(__file__.replace(os.path.basename(__file__), ''))
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+cur = conn.cursor()
+
+def addEntry(name, age):
+    cur.execute("INSERT INTO posts VALUES(?, ?)", (name, age))
+    conn.commit()
+
+def show():
+    cur.execute("SELECT * FROM posts")
+    return cur.fetchall()
+
+def createTable():
+    cur.execute("DROP TABLE posts IF EXISTS")
+    cur.execute(""" CREATE TABLE posts (
+        name TEXT
+        age INTEGER
+    )""")
+    conn.commit()
+
+if __name__ == '__main__':
+    createTable()
